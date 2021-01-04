f2a49a322362a562d445eeee1a1b78e5d8c5fc52
Yaw <yawasamoah13@hotmail.com>
2021-01-04 17:07:17 -0500

    Added firebase firestore

---
+++ script.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAc9-jQbharaduQu6ZxIBl_EFNcNLFlCTA",
+    authDomain: "spar-web.firebaseapp.com",
+    projectId: "spar-web",
+    storageBucket: "spar-web.appspot.com",
+    messagingSenderId: "954715243296",
+    appId: "1:954715243296:web:9c49f0db5fcafeee2d0d45"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
